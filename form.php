d61f1df64a9954555fb43cd826ab15bf4c3f1e8d
dresosh <contact@dresosh.com>
2016-09-09 14:30:13 -0700

    creates new mail form with attachment

--- form.php
+++ form.php
@@ -1,23 +1,11 @@
 <div class="container">
-  <form class="" method="post" enctype="multipart/form-data">
-    <br>
-    <div class="form-group col-sm-6">
-      <input type="file" name="file" class="form-control">
+  <div class="row">
+    <div class="col-sm-6">
+      <form class="main-form" method="post">
+        <div class="form-group">
+          <input type="file" class="form-control" name="file">
+        </div>
+      </form>
     </div>
-    <div class="form-group">
-      <button type="submit" name="submit" class="btn">Submit</button>
-    </div>
-  </form>
-</div>
-
-<div class="container">
-  <?php
-    $submit = $_POST['submit'];
-    $file = $_FILES['file'];
-
-    if (isset($submit)) {
-      print_r($file);
-      
-    }
-  ?>
+  </div>
 </div>
